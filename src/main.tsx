568d48774da547d0289e2a9221a735659db1cea0
silvermuso <silvermuso@gmail.com>
2025-04-03 13:08:34 +0900

    fix: Update router configuration and base URL

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
-import App from './App.tsx'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-
-const basename = process.env.NODE_ENV === 'production' ? '/zes-engineering' : ''
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename={basename}>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 ) 
