b8cdc26aaf8bf5afb9880b57280dab33dddfb2e9
silvermuso <silvermuso@gmail.com>
2025-04-03 10:56:35 +0900

    fix: Update main.tsx to use HashRouter and fix module paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
+import { HashRouter as Router } from 'react-router-dom'
+import App from './App.tsx'
 import './index.css'
 
 const basename = process.env.NODE_ENV === 'production' ? '/zes-engineering' : ''
