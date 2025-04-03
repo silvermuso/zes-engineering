5bc448e903af34c69dda8c7cd0de6f8c31e7fd25
silvermuso <silvermuso@gmail.com>
2025-04-03 10:00:14 +0900

    fix: Add conditional base URL for development and production

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const basename = process.env.NODE_ENV === 'production' ? '/zes-engineering' : ''
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={basename}>
       <App />
     </Router>
   </React.StrictMode>,
